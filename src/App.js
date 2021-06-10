24754a01860d021367b7de76d70d9a67989b481f
Truman <trumanngny@yahoo.com>
2021-06-10 16:35:25 -0400

    fix fetch and input

--- src/App.js
+++ src/App.js
@@ -3,16 +3,27 @@ import React from 'react';
 
 class App extends React.Component{
   
+  constructor(props) {
+    super(props);
+    this.state={
+      cities:[],
+    };
+  }
 
-  
+  getCity()
+  { const zipCode = document.getElementById("zipcode");
+    const url = "http://ctp-zip-api.herokuapp.com/zip/"+zipCode;
+    fetch(url).then(response => response.json()).then(data => {this.setState({cities: data})})
+
+  }
 
   render(){
     return (
     <div>
       <h1>Zip Code Search</h1>
       <form id ="form">
-         <p>Zip Code </p> <input type="text"/>
-         <button type ="button" > Submit</button>
+         <p>Zip Code </p> <input placeholder="12345" type="text"/>
+         <button type ="button" onClick={this.getCity}> Submit</button>
        </form>
     </div>)
   }
