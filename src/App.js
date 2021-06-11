696efa4455d231a78a51cc09bad75202265c3aae
Truman <trumanngny@yahoo.com>
2021-06-10 21:06:38 -0400

    add getCity Components

--- src/App.js
+++ src/App.js
@@ -1,41 +1,58 @@
-import React from 'react';
+import React,{Component} from 'react';
 import City from "./city"
 
-class App extends React.Component{
+class App extends Component{
   
-  constructor(props) {
-    super(props);
-    var buildCity =[];
-    this.state={
+    
+    state={
       cities:[],
+      v: [],
     };
-  }
 
-  getCity()
+
+  getCities = () =>
   { const zipCode = document.getElementById("zipcode").value;
     const url = "http://ctp-zip-api.herokuapp.com/zip/"+zipCode;
     fetch(url).
     then(response => response.json()).
-    //then(data => {console.log(data)}).
-    then(data => {this.setState({cities: data})}).
-    catch(error => console.log("error for zipcode"));
-    console.log(this.state.cities);
+    then(responseJson => {
+      console.log(responseJson);
+      this.setState({cities: responseJson});
+    }).
+    then(error => console.log("error:"));
+    
+    
   }
 
-  getCityBar(){
+  getCityComponents = () =>
+  {
+    
+     for(let i in this.state.cities)
+     {
+       console.log(i);
+       var joined = this.state.v.concat(<City city={this.state.cities[i]}/>);
+       this.setState({ v: joined });
+       console.log(this.state.v[i]);
+       console.log(this.state.cities[i]);
+     }  
+  } 
+  
 
-  }
+  
 
   render(){
+   
 
     return (
     <div>
       <h1>Zip Code Search</h1>
       <form id ="form">
          <p>Zip Code </p> <input id="zipcode" placeholder="12345" type="text"/>
-         <button type ="button" onClick={this.getCity}> Submit</button>
+         <button type ="button" onClick={this.getCities}> Submit</button>
+         <button type ="button" onClick={this.getCityComponents}> sdsds</button>
        </form>
-       {this.getCityBar}
+        {this.state.v}
+       
     </div>)
   }
 }
